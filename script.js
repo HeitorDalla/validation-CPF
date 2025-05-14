a13a2cd793f70987d84f3f77106658d2bbfb98ca
HeitorDalla <heitorvillavilla@gmail.com>
2025-05-13 21:45:08 -0300

    Finalizado a funcao de validacao de cpf

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function validarCPF (cpf) {
 
     // Validação do 1 DV
     for (let i=1; i<=9; i++) {
-        soma += parseInt(cpf.substring(i-1, i)) * (11-i);
+        soma += parseInt(cpf.substring(i - 1, i)) * (11 - i);
     }
 
     resto = (soma * 10) % 11;
@@ -20,4 +20,28 @@ function validarCPF (cpf) {
     if (resto === 10 || resto === 11) {
         resto = 0;
     }
+
+    if (resto !== parseInt(cpf.substring(9, 10))) {
+        return false;
+    }
+
+    // Validação do 2 DV
+    soma = 0;
+
+    for (let i=1; i<=10; i++) {
+        soma += parseInt(cpf.substring(i - 1, i)) * (12 - i);
+    }
+
+    resto = (soma * 10) % 11;
+
+    if (resto === 10 | resto === 11) {
+        resto = 0;
+    }
+
+    if (resto !== parseInt(cpf.substring(10, 11))) {
+        return false;
+    }
+
+    return true;
 }
+
